a9510afb22ac2dc08715d68d63c4de946c83002b
Leonard Siebeneicher <leonard7e@gmail.com>
2019-01-26 18:34:59 +0100

    More flexibility for cpattern

--- src/vec2.h
+++ src/vec2.h
@@ -2,66 +2,72 @@
 #define VEC2_H
 
 // Two dimensional vectors
-struct vec2 {
-  float arr[2];
+struct Vec2 {
+    float arr[2];
+};
+
+Vec2 __operator__add__ (Vec2 a, Vec2 b)
+{
+    float arr[2] = {
+     a.arr[0] + b.arr[0],
+     a.arr[1] + b.arr[1]
+    };
+    Vec2 res = { arr };
+    return res;
+    //return { arr };
 }
 
-vec2 __operator__add__ (vec2 a, vec2 b)
+Vec2 __operator__sub__ (Vec2 a, Vec2 b)
 {
-  float arr[2] = {
-    a.arr[0] + b.arr[0],
-    a.arr[1] + b.arr[1]
-  }
-  return vec2 { arr };
+    float arr[2] = {
+        a.arr[0] - b.arr[0],
+        a.arr[1] - b.arr[1]
+    };
+    Vec2 res = { arr };
+    return res;
 }
 
-vec2 __operator__sub__ (vec2 a, vec2 b)
+Vec2 __operator__neg__ (Vec2 a)
 {
-  float arr[2] = {
-    a.arr[0] - b.arr[0],
-    a.arr[1] - b.arr[1]
-  }
-  return vec2 { arr };
-}
-
-vec2 __operator__neg__ (vec2 a)
-{
-  float arr[2] = {
-    - a.arr[0],
-    - a.arr[1]
-  }
-  return vec2 { arr };
+    float arr[2] = {
+        - a.arr[0],
+        - a.arr[1]
+    };
+    Vec2 res = { arr };
+    return res;
 }
 
 // Matrices for two dimensions
 
 /* Indices of matrix
-  0   1
-  2   3
+0   1
+2   3
 */
-struct matr2 {
-  float arr[4];
+struct Matr2 {
+    float arr[4];
+};
+
+Matr2 __operator__mul__ (Matr2 a, Matr2 b)
+{
+    float arr[4] = {
+        a.arr[0]*b.arr[0] + a.arr[1]*b.arr[2],
+        a.arr[0]*b.arr[1] + a.arr[1]*b.arr[3],
+
+        a.arr[2]*b.arr[0] + a.arr[3]*b.arr[2],
+        a.arr[2]*b.arr[1] + a.arr[3]*b.arr[3]
+    };
+    Matr2 res = { arr };
+    return res;
 }
 
-matr2 __operator__mul__ (matr2 a, matr2 b)
+Vec2 transform (Matr2 m, Vec2 x)
 {
-  float arr[4] = {
-    a.arr[0]*b.arr[0] + a.arr[1]*b.arr[2],
-    a.arr[0]*b.arr[1] + a.arr[1]*b.arr[3],
-
-    a.arr[2]*b.arr[0] + a.arr[3]*b.arr[2],
-    a.arr[2]*b.arr[1] + a.arr[3]*b.arr[3]
-  }
-  return matr2 { arr };
-}
-
-vec2 transform (matr2 m, vec2 x)
-{
-  float arr[2] = {
-    m[0]*x[0]+m[1]*x[1],
-    m[2]*x[0]+m[3]*x[1]
-  };
-  return vec2 { arr };
+    float arr[2] = {
+        m.arr[0]*x.arr[0]+m.arr[1]*x.arr[1],
+        m.arr[2]*x.arr[0]+m.arr[3]*x.arr[1]
+    };
+    Vec2 res = { arr };
+    return res;
 }
 
 #endif
